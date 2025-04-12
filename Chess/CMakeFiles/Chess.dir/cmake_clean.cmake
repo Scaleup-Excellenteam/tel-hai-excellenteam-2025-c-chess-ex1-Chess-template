13e423aeb29770e0a1470ecd1b5617636f28c333
Daniel Kusai <dan.kusai@gmail.com>
2025-04-12 11:28:59 +0300

    CMade

---
+++ Chess/CMakeFiles/Chess.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess.dir/link.d"
+  "CMakeFiles/Chess.dir/src/Chess.cpp.o"
+  "CMakeFiles/Chess.dir/src/Chess.cpp.o.d"
+  "CMakeFiles/Chess.dir/src/main.cpp.o"
+  "CMakeFiles/Chess.dir/src/main.cpp.o.d"
+  "Chess"
+  "Chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
