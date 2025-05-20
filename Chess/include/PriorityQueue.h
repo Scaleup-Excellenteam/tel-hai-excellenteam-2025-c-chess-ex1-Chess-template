af6c6969b5dbc2ea464f9ddcd0221f0a16a65a23
dannightmare <dan.kusai.star.123234@gmail.com>
2025-05-20 20:36:37 +0300

    Added PriorityQueue full impl

--- Chess/include/PriorityQueue.h
+++ Chess/include/PriorityQueue.h
@@ -1,11 +1,16 @@
 #include <list>
 template <class T>
+struct Comparator {
+    int operator() (const T& l, const T& r);
+};
+
+template <class T, class Comp = Comparator<T> >
 class PriorityQueue
 {
 private:
-    std::list<T> priority_queue;
+    std::list<T> queue;
 
 public:
-    const T &poll() const;
+    T poll();
     void push(const T &item);
 };
--- Chess/src/PriorityQueue.cpp
+++ Chess/src/PriorityQueue.cpp
@@ -1,7 +1,26 @@
 #include "PriorityQueue.h"
+#include <algorithm>
 
 template <class T>
-const T &PriorityQueue<T>::poll() const
+int Comparator<T>::operator()(const T &l, const T &r)
 {
-    return priority_queue.front();
+    return r - l;
+}
+
+template <class T, class Comp>
+T PriorityQueue<T, Comp>::poll()
+{
+    T ret = queue.front();
+    queue.pop_front();
+    return ret;
+}
+
+template <class T, class Comp>
+void PriorityQueue<T, Comp>::push(const T &item)
+{
+    Comp comp;
+    queue.insert(
+        std::find_if(queue.begin(), queue.end(),
+                     [&item, &comp](const T &r) { return comp(item, r) > 0; }),
+        item);
 }
